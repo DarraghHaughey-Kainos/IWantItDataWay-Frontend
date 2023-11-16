d8d004dc01d07a764ac8aaca637b78fd417b7bda
Stephen Hanna <stephenh126_@hotmail.co.uk>
2023-11-16 17:00:41 +0000

    changed

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ const nunjucks = require('nunjucks');
 import { Request, Response, NextFunction } from 'express';
 import session = require('express-session');
 
-console.log("SECRETS: "+process.env)
+console.log("SECRETS: "+JSON.stringify(process.env));
 
 const app = express();
 const authMiddleware = require('./middleware/auth');
